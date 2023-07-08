481fe07b11bed7c066ed27cf26afa32d171ad181
Shamman626 <m15302938395@163.com>
2023-07-08 18:08:19 +0800

    管理中心页面完成（不包括增加人员功能）

--- src/components/menu/Management/addOld.vue
+++ src/components/menu/Management/addOld.vue
@@ -1,10 +1,24 @@
 <template>
-<div>增加老人信息</div>
+  <el-popover
+      placement="top"
+      width="160"
+      v-model="visible">
+    <p>这是一段内容这是一段内容确定删除吗？</p>
+    <div style="text-align: right; margin: 0">
+      <el-button size="mini" type="text" @click="visible = false">取消</el-button>
+      <el-button type="primary" size="mini" @click="visible = false">确定</el-button>
+    </div>
+    <el-button slot="reference">删除</el-button>
+  </el-popover>
 </template>
 
 <script>
 export default {
-  name: "addOld"
+  data() {
+    return {
+      visible: false,
+    };
+  }
 }
 </script>
 
